529f9b6d15d3ba896bb596b60e50a1f5d30d7360
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-06-11 13:38:24 +0630

    #add: catchAsyncError middleware & validation & mongoose id error handler

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -1,107 +1,74 @@
 import Room from "../models/room";
 import ErrorHandler from "../utils/errorHandler";
+import catchAsyncError from "../middlewares/catchAsyncError";
 
 // GET => /api/rooms
-const getAllRooms = async (req, res) => {
-    try {
-        const rooms = await Room.find();
-        res.status(200).json({
-            status: "success",
-            results: rooms.length,
-            data: {
-                data: rooms,
-            },
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: "fail",
-            error: err,
-        });
-    }
-};
+const getAllRooms = catchAsyncError(async (req, res, next) => {
+    const rooms = await Room.find();
+    res.status(200).json({
+        status: "success",
+        results: rooms.length,
+        data: {
+            data: rooms,
+        },
+    });
+});
 
 // POST => /api/rooms
-const createNewRoom = async (req, res) => {
-    try {
-        const newRoom = await Room.create(req.body);
-        res.status(201).json({
-            status: "success",
-            data: {
-                data: newRoom,
-            },
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: "fail",
-            error: err,
-        });
-    }
-};
+const createNewRoom = catchAsyncError(async (req, res) => {
+    const newRoom = await Room.create(req.body);
+    res.status(201).json({
+        status: "success",
+        data: {
+            data: newRoom,
+        },
+    });
+});
 
 // GET => /api/rooms/[id]
-const getSingleRoom = async (req, res, next) => {
-    try {
-        const room = await Room.findById(req.query.id);
-        if (!room) {
-            return next(new ErrorHandler("Room not found with that id", 404));
-        }
-        res.status(200).json({
-            status: "success",
-            data: {
-                data: room,
-            },
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: "fail",
-            error: err,
-        });
+const getSingleRoom = catchAsyncError(async (req, res, next) => {
+    const room = await Room.findById(req.query.id);
+    if (!room) {
+        return next(new ErrorHandler("Room not found with that id", 404));
     }
-};
+    res.status(200).json({
+        status: "success",
+        data: {
+            data: room,
+        },
+    });
+});
 
 // PATCH => /api/rooms/[id]
-const updateSingleRoom = async (req, res, next) => {
-    try {
-        const updatedRoom = await Room.findByIdAndUpdate(req.query.id, req.body, {
-            new: true,
-            runValidators: true,
-        });
-        if (!updatedRoom) {
-            return next(new ErrorHandler("Room not found with that id", 404));
-        }
-        res.status(200).json({
-            status: "success",
-            data: {
-                data: updatedRoom,
-            },
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: "fail",
-            error: err,
-        });
+const updateSingleRoom = catchAsyncError(async (req, res, next) => {
+    const updatedRoom = await Room.findByIdAndUpdate(req.query.id, req.body, {
+        new: true,
+        runValidators: true,
+    });
+    if (!updatedRoom) {
+        return next(new ErrorHandler("Room not found with that id", 404));
     }
-};
+    res.status(200).json({
+        status: "success",
+        data: {
+            data: updatedRoom,
+        },
+    });
+});
 
 // DELETE => /api/rooms/[id]
-const deleteSingleRoom = async (req, res, next) => {
-    try {
-        const deletedRoom = await Room.findByIdAndDelete(req.query.id);
+const deleteSingleRoom = catchAsyncError(async (req, res, next) => {
+    const deletedRoom = await Room.findByIdAndDelete(req.query.id);
 
-        if (!deletedRoom) {
-            return next(new ErrorHandler("Room not found with that id", 404));
-        }
-        res.status(204).json({
-            status: "success",
-            data: {
-                data: deletedRoom,
-            },
-        });
-    } catch (err) {
-        res.status(400).json({
-            status: "fail",
-            error: err,
-        });
+    if (!deletedRoom) {
+        return next(new ErrorHandler("Room not found with that id", 404));
     }
-};
+    res.status(204).json({
+        status: "success",
+        data: {
+            data: deletedRoom,
+        },
+    });
+});
+
 export { getAllRooms, createNewRoom, getSingleRoom, updateSingleRoom, deleteSingleRoom };
--- middlewares/globalError.js
+++ middlewares/globalError.js
@@ -1,10 +1,54 @@
-export default (err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.statusText = err.statusText || "error";
+import ErrorHandler from "../utils/errorHandler";
+
+const handleDevError = (err, res) => {
     res.status(err.statusCode).json({
         status: err.statusText,
+        name: err.name,
         error: err,
         message: err.message,
         stack: err.stack,
     });
+};
+
+const handleProdError = (err, res) => {
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.statusText,
+            message: err.message,
+        });
+    } else {
+        res.status(500).json({
+            status: "error",
+            message: "Oops! Something went very wrong.",
+        });
+    }
+};
+
+const handleCastError = (err) => new ErrorHandler(`Invalid: ${err.path}: ${err.value}`, 400);
+
+const handleValidationError = (err) => {
+    const message = Object.values(err.errors)
+        .map((key) => key.message)
+        .join(" .");
+    return new ErrorHandler(message, 400);
+};
+
+export default (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.statusText = err.statusText || "error";
+    if (process.env.NODE_ENV === "development") {
+        handleDevError(err, res);
+    } else if (process.env.NODE_ENV === "production") {
+        
+        // mongoose invalid id with wrong format error
+        if (err.name === "CastError") {
+            err = handleCastError(err);
+        }
+
+        // mongoose validation error
+        if (err.name === "ValidationError") {
+            err = handleValidationError(err);
+        }
+        handleProdError(err, res);
+    }
 };
