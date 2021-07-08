de6fb8a2cbce624fd421e5ab4fc302a09c75c5f1
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-07-08 18:05:27 +0630

    #add: testing deployment

--- utils/valhallaAxios.js
+++ utils/valhallaAxios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const urls = {
     development: "http://localhost:3000",
+    production: "https://hotelvalhalla.vercel.app",
 };
 export default axios.create({
     baseURL: urls[process.env.NODE_ENV],
