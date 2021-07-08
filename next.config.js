b1d0e0abcaaef9d3e2fb16b28cce588393a7f088
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-07-08 18:11:07 +0630

    #add: env

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ module.exports = {
         };
         return config;
     },
+    env: {
+        DB_CONNECTION_STRING_PROD:
+            "mongodb+srv://valhalla:valhallanextjs@cluster0.je3fi.mongodb.net/hotel?retryWrites=true&w=majority",
+    },
     images: {
         domains: ["res.cloudinary.com"],
     },
