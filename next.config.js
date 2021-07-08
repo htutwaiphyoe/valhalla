e155c51d5e7ec2a7883d521acbab84259c4d4d9a
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-07-08 18:14:14 +0630

    #delete: env

--- next.config.js
+++ next.config.js
@@ -5,10 +5,6 @@ module.exports = {
         };
         return config;
     },
-    env: {
-        DB_CONNECTION_STRING_PROD:
-            "mongodb+srv://valhalla:valhallanextjs@cluster0.je3fi.mongodb.net/hotel?retryWrites=true&w=majority",
-    },
     images: {
         domains: ["res.cloudinary.com"],
     },
