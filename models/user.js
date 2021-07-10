50f6152d90a3a2137775bd1e2cfdccdd1f2c69b0
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-07-10 10:10:33 +0630

    #add: user mondel

---
+++ models/user.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import validator from "validator";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please enter your name"],
+        maxLength: [30, "Your name cannot exceed 30 characters"],
+    },
+    email: {
+        type: String,
+        required: [true, "Please enter your email"],
+        unique: true,
+        validate: [validator.isEmail, "Please enter valid email address"],
+    },
+    password: {
+        type: String,
+        required: [true, "Please enter your password"],
+        minLength: [8, "Your password must be longer than 8 characters"],
+        select: false,
+    },
+
+    avatar: {
+        public_id: {
+            type: String,
+            required: true,
+        },
+        url: {
+            type: String,
+            required: true,
+        },
+    },
+
+    role: {
+        type: String,
+        default: "user",
+        enum: {
+            values: ["user", "admin"],
+            message: "{VALUE} is not supported.",
+        },
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+});
+
+export default mongoose.models.User || mongoose.model("User", userSchema);
