55eb552089c79169852cb5361a59751ca3a5b40d
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-07-12 11:26:59 +0630

    #add: uploading image with cloudinary

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,22 @@
+import cloudinary from "cloudinary";
+
 import User from "../models/user";
 import catchAsyncError from "../middlewares/catchAsyncError";
 import ErrorHandler from "../utils/errorHandler";
 
+// cloudinary configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 export const signUp = catchAsyncError(async (req, res) => {
+    const result = await cloudinary.v2.uploader.upload(req.body.avatar, {
+        folder: "valhalla/avatar",
+        width: "150",
+        crop: "scale",
+    });
     const { name, email, password } = req.body;
 
     const newUser = await User.create({
@@ -10,8 +24,8 @@ export const signUp = catchAsyncError(async (req, res) => {
         email,
         password,
         avatar: {
-            public_id: "PUBLIC_ID",
-            url: "URL",
+            public_id: result.public_id,
+            url: result.secure_url,
         },
     });
 
